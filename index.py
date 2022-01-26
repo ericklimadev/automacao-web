b5a92e41dcd8260f7d6a1abebb2e2d57c3b3f80b
Erick Lima <dev.ericklima@gmail.com>
2022-01-26 13:23:22 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+# Abrir um navegador
+navegador = webdriver.Chrome()
+
+navegador.get("https://www.google.com/")
+
+# Pegar a cotação do Dólar
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dólar")
+
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_dolar = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value") 
+print(cotacao_dolar)
+
+# Pegar a cotação do Euro
+navegador.get("https://www.google.com/")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação euro")
+navegador.find_element(By.XPATH,
+    '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+
+cotacao_euro = navegador.find_element(By.XPATH,
+    '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute("data-value")
+print(cotacao_euro)
+
+# Pegar a cotação do Ouro
+navegador.get("https://www.melhorcambio.com/ouro-hoje")
+
+cotacao_ouro = navegador.find_element(By.XPATH, '//*[@id="comercial"]').get_attribute("value")
+cotacao_ouro = cotacao_ouro.replace(",", ".")
+print(cotacao_ouro)
+
+navegador.quit()
+
+tabela = pd.read_excel("Produtos.xlsx")
+
+tabela.loc[tabela["Moeda"] == "Dólar", "Cotação"] = float(cotacao_dolar)
+tabela.loc[tabela["Moeda"] == "Euro", "Cotação"] = float(cotacao_euro)
+tabela.loc[tabela["Moeda"] == "Ouro", "Cotação"] = float(cotacao_ouro)
+
+# Atualizar o preço base reais (preço base original * cotação)
+tabela["Preço de Compra"] = tabela["Preço Original"] * tabela["Cotação"]
+
+# Atualizar o preço final (preço base reais * Margem)
+tabela["Preço de Venda"] = tabela["Preço de Compra"] * tabela["Margem"]
+
+# Salvar os novos preços dos produtos em um novo arquivo
+tabela.to_excel("Produtos Novo.xlsx", index=False)
